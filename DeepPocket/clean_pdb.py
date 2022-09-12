30288691ac6cec14d7f15d3ded5dfb844a32c0e6
siva durga <57574795+sivadurga-web@users.noreply.github.com>
2022-09-13 00:09:14 +0530

    Add files via upload

--- DeepPocket/clean_pdb.py
+++ DeepPocket/clean_pdb.py
@@ -18,10 +18,15 @@ def clean_pdb(input_file,output_file):
     Takes a PDB file and removes hetero atoms from its structure.
     First argument is path to original file, second argument is path to generated file
     '''
-    pdb = PDBParser().get_structure("protein", input_file)
-    io = PDBIO() # pylint: disable=C0103
-    io.set_structure(pdb)
-    io.save(output_file, NonHetSelect())
+    try:
+        pdb = PDBParser().get_structure("protein", input_file)
+        io = PDBIO() # pylint: disable=C0103
+        io.set_structure(pdb)
+        io.save(output_file, NonHetSelect())
+        return output_file
+    except FileNotFoundError:
+        assert FileNotFoundError
+        return None
 
 if __name__ == '__main__':
     clean_pdb(sys.argv[1],sys.argv[2])
--- DeepPocket/get_centers.py
+++ DeepPocket/get_centers.py
@@ -14,27 +14,33 @@ def get_centers(directory):
     outputs a file containining candidate pocket centers in that directory
     '''
     # pylint: disable=R1732,W1514,W1401
-    bary = open(directory+'/bary_centers.txt','w')
-    for file in os.listdir(directory):
-        centers = []
-        masses = []
-        if file.endswith('vert.pqr'):
-            num = int(re.search(r'\d+', file).group())
-            file_p = open(directory+'/'+file)
-            for line in file_p:
-                if line.startswith('ATOM'):
-                    center=list(map(float,
-                                re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?",
-                                 ' '.join(line.split()[5:]))))[:3]
-                    mass=float(line.split()[-1])
-                    centers.append(center)
-                    masses.append(mass)
-            centers=np.asarray(centers)
-            masses=np.asarray(masses)
-            xyzm = (centers.T * masses).T
-            xyzm_sum = xyzm.sum(axis=0) # find the total xyz*m for each element
-            cog = xyzm_sum / masses.sum()
-            bary.write(str(num) + '\t' + str(cog[0])+'\t' + str(cog[1]) + '\t' + str(cog[2]) + '\n')
+    try:
+        bary = open(directory+'/bary_centers.txt','w')
+        for file in os.listdir(directory):
+            centers = []
+            masses = []
+            if file.endswith('vert.pqr'):
+                num = int(re.search(r'\d+', file).group())
+                file_p = open(directory+'/'+file)
+                for line in file_p:
+                    if line.startswith('ATOM'):
+                        center=list(map(float,
+                        re.findall("[-+]?[.]?[\d]+(?:,\d\d\d)*[\.]?\d*(?:[eE][-+]?\d+)?",
+                                    ' '.join(line.split()[5:]))))[:3]
+                        mass=float(line.split()[-1])
+                        centers.append(center)
+                        masses.append(mass)
+                centers=np.asarray(centers)
+                masses=np.asarray(masses)
+                xyzm = (centers.T * masses).T
+                xyzm_sum = xyzm.sum(axis=0) # find the total xyz*m for each element
+                cog = xyzm_sum / masses.sum()
+                bary.write(str(num) + '\t' + str(cog[0])+'\t'
+                     + str(cog[1]) + '\t' + str(cog[2]) + '\n')
+    except FileNotFoundError:
+        assert False, f'No such directory: {directory}'
+    except Exception as eve:
+        assert False, f'Error: {eve}'
 
 if __name__ == '__main__':
     get_centers(sys.argv[1])
